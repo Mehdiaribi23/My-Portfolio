11b989c40d6a65f4d70069add3c93ec32050baf7
Mehdiaribi23 <aribimehdi85@gmail.com>
2023-12-28 17:04:39 +0100

    changé alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,20 +1,20 @@
 const Alert = ({ type, text }) => {
   return (
-    <div className="absolute top-10 left-0 right-0 flex justify-center items-center">
+    <div className="fixed top-10 left-1/2 transform -translate-x-1/2">
       <div
         className={`p-2 ${
           type === "danger" ? "bg-red-800" : "bg-blue-800"
-        } items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex`}
+        } text-indigo-100 leading-none rounded-full flex items-center sm:min-w-full sm:px-4 md:max-w-md lg:max-w-xl`}
         role="alert"
       >
         <p
           className={`flex rounded-full ${
             type === "danger" ? "bg-red-500" : "bg-blue-500"
-          } uppercase px-2 py-1 text-xs font-semibold mr-3`}
+          } uppercase px-2 py-1 text-xs font-semibold mr-2`}
         >
           {type === "danger" ? "Failed" : "Success"}
         </p>
-        <p className="mr-2 text-left">{text}</p>
+        <p className="text-left text-xs">{text}</p>
       </div>
     </div>
   );
